e8c605cff776a23d8b4de4714bd8053f8ca7b71b
Mira Heckmann <miraheckmann@gmail.com>
2021-04-29 12:57:34 -0400

    installs and configures bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 6; 
 
 const userSchema = new Schema({
     userName: {
@@ -13,5 +15,15 @@ const userSchema = new Schema({
     },
     password: String, 
 }, { timestamps: true });
+
+//before a user document is saved to the database the password is hashed
+//hooks into the cycle, before it saves calls function(next)
+
+userSchema.pre('save', function (next) {
+    const user = this;
+        //this keyword points to the document that is about to be saved 
+    if (!user.isModified("password")) return next();
+        //dont hash the password if it hasnt been modified
+})
 
 module.exports = mongoose.model('User', userSchema);
